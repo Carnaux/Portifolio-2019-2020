4723f3363866c96972f45cfebb0da2441d65ee9c
Carnaux <danielfeenandes@gmail.com>
2019-03-17 03:10:53 -0300

    New Site

---
+++ index.js
@@ -0,0 +1,36 @@
+var scene = new THREE.Scene();
+scene.background = new THREE.Color("rgb(35,35,35)");
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+let renderDiv = document.getElementById("renderDiv");
+renderDiv.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+cube.material.wireframe = true;
+scene.add( cube );
+
+camera.position.z = 5;
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
--- j1.js
+++
@@ -1,9 +0,0 @@
-function openAbout() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("about").style.display = "block";
-}
-
-function openHome() {
-  document.getElementById("home").style.display = "block";
-  document.getElementById("about").style.display = "none";
-}
